25326da13a35118d09396a75b6af3a3f648edd67
Cristian <metacris93@gmail.com>
2016-12-01 12:10:59 -0500

    se anadio la funconalidad del bluetooth, falta hacer las pruebas con el baston

--- baston_para_ciegos/baston_para_ciegos.ino
+++ baston_para_ciegos/baston_para_ciegos.ino
@@ -1,12 +1,17 @@
+#include <SoftwareSerial.h>
+
 #define pin_echo1 6
 #define pin_trig1 7
 #define pin_echo2 3
 #define pin_trig2 4
 #define led 13
+SoftwareSerial B(10, 11); // RX, TX
 long duracion1, distancia1, duracion2, distancia2;
-int flag=0;
+int flag=0, estado;
+
 void setup() {
   Serial.begin(9600);
+  B.begin(9600);
   pinMode(pin_echo1, INPUT);
   pinMode(pin_trig1, OUTPUT);
   pinMode(pin_echo2, INPUT);
@@ -21,18 +26,20 @@ void primer_sensor(){
   digitalWrite(pin_trig1,LOW);
   duracion1 = pulseIn(pin_echo1,HIGH);
   distancia1 = (duracion1/2)/29; //la distancia esta en centimetros 
-  if( distancia1 <= 50 && distancia1 > 10){
-    Serial.println("Sensor1...");
-    Serial.print(distancia1);
-    Serial.println(" cm");
-    flag = 1;
+  if( distancia1 <= 70 && distancia1 > 10){
+      Serial.println("Sensor1...");
+      Serial.print(distancia1);
+      Serial.println(" cm");
+      flag = 1;
       digitalWrite(led,HIGH);
+      B.println(1);
   }else if(flag != 2){
       Serial.println("...x1");
-    Serial.print(distancia1);
-    Serial.println(" cm");
-    flag = 0;
-    digitalWrite(led,LOW);
+      Serial.print(distancia1);
+      Serial.println(" cm");
+      flag = 0;
+      digitalWrite(led,LOW);
+      B.println(0);
   }
 }
 
@@ -44,80 +51,27 @@ void segundo_sensor(){
   duracion2 = pulseIn(pin_echo2,HIGH);
   distancia2 = (duracion2/2)/29; //la distancia esta en centimetros 
   
-   if( distancia2 > 20){
-    Serial.println("Sensor2...");
-    Serial.print(distancia2);
-    Serial.println(" cm");
-    flag =2;
+   if( distancia2 > 30){
+      Serial.println("Sensor2...");
+      Serial.print(distancia2);
+      Serial.println(" cm");
+      flag =2;
       digitalWrite(led,HIGH);
+      B.println(2);
   }else if(flag != 1){
       Serial.println("...x2");
-    Serial.print(distancia2);
-    Serial.println(" cm");
-    flag = 0;
-    digitalWrite(led,LOW);
+      Serial.print(distancia2);
+      Serial.println(" cm");
+      flag = 0;
+      digitalWrite(led,LOW);
+      B.println(0);
   }
 }
+
 void loop() {
   Serial.println("\n");
   primer_sensor();
-  delay(500);
-  segundo_sensor();
- /* //digitalWrite(pin_trig1, LOW);
-  //delayMicroseconds(2);
-  digitalWrite(pin_trig2, LOW);
-  delayMicroseconds(2);
-  //digitalWrite(pin_trig1, HIGH);
-  //delayMicroseconds(10);
-  digitalWrite(pin_trig2, HIGH);
-  delayMicroseconds(10);
-  //digitalWrite(pin_trig1, LOW);
-  digitalWrite(pin_trig2, LOW);
-  
-  //duracion1 = pulseIn(pin_echo1,HIGH);
-  //distancia1 = (duracion1/2)/29; //la distancia esta en centimetros
-  
-  duracion2 = pulseIn(pin_echo2,HIGH);
-  distancia2 = (duracion2/2)/29; //la distancia esta en centimetros
-  //Serial.println("Sensor 1:");
-  //Serial.println(duracion1);
-  Serial.println("Sensor 2:");
-  Serial.println(distancia2);*/
-  /*if( distancia1 <= 50 && distancia1 > 10 && distancia2 < 20 ){ // si distancia es mayor a 500 cm o menor a 0, no hace nada    
-    Serial.println("Alarma1...");
-    Serial.print(distancia1);
-    Serial.println(" cm");
-    digitalWrite(led,HIGH);
-  }
-  if(distancia2 > 20 && !(distancia1 <= 50 && distancia1 > 10)){
-    Serial.println("Alarma2...");
-    Serial.print(distancia2);
-    Serial.println(" cm");
-    digitalWrite(led,HIGH);
-  }
-  if( distancia2 < 20 && !(distancia1 <= 50 && distancia1 > 10) ){
-    Serial.println("...");
-    Serial.print(distancia2);
-    Serial.println(" cm");
-    digitalWrite(led,LOW); 
-  }*/
-  
-  /*else if(distancia2 >=20){
-    Serial.println("Alarma2...");
-    Serial.print(distancia2);
-    Serial.println(" cm");
-    digitalWrite(led,HIGH);  
-  }else if (distancia2 < 20){
-    Serial.println("...x2");
-    Serial.print(distancia2);
-    Serial.println(" cm");
-    digitalWrite(led,LOW);
-  }else if (distancia1<= 10 || distancia1 > 50){
-    //Serial.println("---");
-    //Serial.print(distancia1);
-    //Serial.println(" cm");
-    digitalWrite(led,LOW); 
-  }*/
-  
+  delay(1000);
+  segundo_sensor();  
   delay(400);
 }
